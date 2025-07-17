8ceefa0c8d272f76e8caf3f5b06fbcc496c861c5
Vaibhav Maurya <vaibhav142023@gmail.com>
2025-07-17 15:52:49 +0000

    variables

---
+++ basicsOfjs/01_variables.js
@@ -0,0 +1,7 @@
+const AccountId = 312414513;
+let accountEmail = "maurya@144123";
+var Accountpassword = "323445342343";
+accountcity = "jaipur";
+let jai_allah;
+
+console.table([AccountId,accountEmail,Accountpassword,accountcity,accountcity,jai_allah]);
